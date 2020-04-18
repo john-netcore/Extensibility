8db90b85f6b97d8c7cce5e3cade57853186218c4
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-18 17:31:33 +0200

    Implemented test RetrievePeople.

--- test/PersonRepository.Service.Tests/PersonRepositoryServiceShould.cs
+++ test/PersonRepository.Service.Tests/PersonRepositoryServiceShould.cs
@@ -1,14 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using PersonRepository.Interface;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace PersonRepository.Service.Tests
 {
     public class PersonRepositoryServiceShould
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ITestOutputHelper _output;
 
+        public PersonRepositoryServiceShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void RetrievePeople()
+        {
+            IPersonRepository sut = new ServiceRepository();
+
+            List<Person> persons = sut.GetPeople().ToList();
+            foreach (var person in persons)
+            {
+                _output.WriteLine(person.Id + " " + person.FirstName);
+            }
+            Assert.Equal(10, persons.Count());
         }
     }
 }
