f93aa45d8faa408d6e27b9e29ad2491fc1358670
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-18 09:27:03 +0200

    Argument fix.

--- src/Repositories/PersonRepository.Interface/IPersonRepository.cs
+++ src/Repositories/PersonRepository.Interface/IPersonRepository.cs
@@ -7,7 +7,7 @@ namespace PersonRepository.Interface
         IEnumerable<Person> GetPeople();
         Person GetPerson(int id);
         void AddPerson(Person newPerson);
-        void UpdatePerson(Person updatedPerson);
+        void UpdatePerson(int id, Person updatedPerson);
         void DeletePerson(int id);
     }
 }
