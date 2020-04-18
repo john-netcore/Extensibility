c678d8cf6590432d7e8c6297a17dd1466e3be64d
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-18 17:31:10 +0200

    Added properties to Person class.

--- src/Repositories/PersonRepository.Interface/Person.cs
+++ src/Repositories/PersonRepository.Interface/Person.cs
@@ -4,5 +4,10 @@ namespace PersonRepository.Interface
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StartDate { get; set; }
+        public int Rating { get; set; }
     }
 }
