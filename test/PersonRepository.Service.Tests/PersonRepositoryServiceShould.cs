b2c3ce2a76a06a1805ef3fef069b8271172b5e37
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-18 16:59:37 +0200

    Added PersonRepository.Service.Tests project.

---
+++ test/PersonRepository.Service.Tests/PersonRepositoryServiceShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PersonRepository.Service.Tests
+{
+    public class PersonRepositoryServiceShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
