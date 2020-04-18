e530be191f845b8cd15489b7d9d6a96d972295eb
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-18 17:58:57 +0200

    Added PersonRepository.CSV project.

---
+++ src/Repositories/PersonRepository.CSV/CSVRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonRepository.Interface;
+
+namespace PersonRepository.CSV
+{
+    public class CSVRepository : IPersonRepository
+    {
+        public IEnumerable<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(int id, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
