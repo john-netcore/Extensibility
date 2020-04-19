5206aea75d5cb9a491cdcfd3bbfe3739b3fcd7a4
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-19 11:40:56 +0200

    GetPeople from csv file implemented.

--- src/Repositories/PersonRepository.CSV/CSVRepository.cs
+++ src/Repositories/PersonRepository.CSV/CSVRepository.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
 using PersonRepository.Interface;
 
 namespace PersonRepository.CSV
 {
     public class CSVRepository : IPersonRepository
     {
+        string _path = "people.csv";
+
         public IEnumerable<Person> GetPeople()
         {
-            throw new NotImplementedException();
+            IEnumerable<Person> result;
+            if (File.Exists(_path))
+            {
+                using (var reader = new StreamReader(_path))
+                {
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        result = csv.GetRecords<Person>();
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("The csv file could not be found", nameof(_path));
+            }
+
+            return result;
         }
 
         public void AddPerson(Person newPerson)
