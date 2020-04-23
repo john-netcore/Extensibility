330f55169ac6b5b818d19e82244350e85dc4f36e
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-23 15:02:53 +0200

    Created class RepositoryFactory.

--- src/Repositories/PersonRepository.Factory/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PersonRepository.Factory
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Repositories/PersonRepository.Factory/RepositoryFactory.cs
@@ -0,0 +1,35 @@
+﻿using PersonRepository.Interface;
+using PersonRepository.CSV;
+using PersonRepository.Service;
+using System;
+
+namespace PersonRepository.Factory
+{
+    public static class RepositoryFactory
+    {
+        public static IPersonRepository GetRepository(RepositoryType repositoryType)
+        {
+            IPersonRepository repository;
+
+            switch (repositoryType)
+            {
+                case RepositoryType.CSVRepository:
+                    repository = new CSVRepository();
+                    break;
+                case RepositoryType.ServiceRepository:
+                    repository = new ServiceRepository();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid repository type");
+            }
+
+            return repository;
+        }
+    }
+
+    public enum RepositoryType
+    {
+        CSVRepository,
+        ServiceRepository
+    }
+}
